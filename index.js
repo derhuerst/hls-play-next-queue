e077c4a29df244c5f96e1b9ac5c26faabe675948
Jannis R <mail@jannisr.de>
2017-08-12 15:53:55 +0200

    adapt for express

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 'use strict'
 
 const randomId = require('shortid').generate
-const {parse} = require('url')
 
 const findTitle = require('./lib/find-title')
 const generatePlaylist = require('./lib/generate-playlist')
 
-const invalid = (res, msg) => {
-	res.statusCode = 400
-	res.end(msg)
-}
-
-const serverError = (res, msg) => {
-	res.statusCode = 500
-	res.end(msg)
+const err = (msg, code) => {
+	err = 'string' === typeof err ? new Error(err) : err
+	err.statusCode = code
+	return err
 }
 
 const createQueue = () => {
@@ -25,40 +20,37 @@ const createQueue = () => {
 		if (i >= 0) queue.splice(i, 1)
 	}
 
-	const add = (req, res, query) => {
-		const filename = query.file // todo: use HTTP body
-		if (!filename) return invalid(res, 'invalid file parameter')
+	const add = (req, res, next) => {
+		const filename = req.query.file
+		if (!filename) return err('invalid file parameter', 400)
 
 		findTitle(filename, (err, data) => {
-			if (err) return serverError(res, err.message)
+			if (err) return err(err, 500)
 
 			data = Object.assign({id: randomId(), sequence: sequence++}, data)
 			if (queue.length > 0) {
 				const last = queue[queue.length - 1]
 				data.start = last.start + last.duration
 			} else data.start = Date.now()
-
 			queue.push(data)
-			setTimeout(removeFromQueue, data.duration, data.id)
 
-			res.statusCode = 201
-			res.end('ok')
+			setTimeout(() => {
+				const i = queue.findIndex(item => item.id === data.id)
+				if (i >= 0) queue.splice(i, 1)
+			}, data.duration)
+
+			res.status(201)
+			res.type('text/plain')
+			res.send('ok')
 		})
 	}
 
 	const list = (req, res) => {
-		// todo: mime type
-		res.end(generatePlaylist(queue))
+		res.type('application/vnd.apple.mpegurl')
+		res.send(generatePlaylist(queue))
 	}
 
-	const onRequest = (req, res) => {
-		const {pathname, query} = parse(req.url, true)
-
-		if (req.method === 'POST' && pathname === '/') add(req, res, query)
-		else if (req.method === 'GET' && pathname === '/') list(req, res)
-	}
-
-	return onRequest
+	return {add, list}
 }
 
 module.exports = createQueue
